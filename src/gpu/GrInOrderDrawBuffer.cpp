Samsung/skia

commit 6d7c5141bc7a5b2b2523da95f7e7521d1db59b51

Moved paint color to vertex colors for batched rects
https://codereview.appspot.com/6620045/



git-svn-id: https://skia.googlecode.com/svn/trunk@5830 2bbb7eff-a529-9590-31e7-b0007b416f81

src/gpu/GrInOrderDrawBuffer.cpp
@@ -91,6 +91,11 @@ void GrInOrderDrawBuffer::drawRect(const GrRect& rect,
 
         bool appendToPreviousDraw = false;
         GrVertexLayout layout = GetRectVertexLayout(srcRects);
+
+        // When we batch rects we store the color at each vertex in order
+        // to allow batching when only the draw color is changing (the usual case)
+        layout |= kColor_VertexLayoutBit;
+
         AutoReleaseGeometry geo(this, layout, 4, 0);
         if (!geo.succeeded()) {
             GrPrintf("Failed to get space for vertices!\n");
@@ -99,7 +104,7 @@ void GrInOrderDrawBuffer::drawRect(const GrRect& rect,
         GrMatrix combinedMatrix = drawState->getViewMatrix();
         // We go to device space so that matrix changes allow us to concat
         // rect draws. When the caller has provided explicit source rects
-        // then we don't want to modify the sampler matrices. Otherwise we do
+        // then we don't want to modify the sampler matrices. Otherwise
         // we have to account for the view matrix change in the sampler
         // matrices.
         uint32_t explicitCoordMask = 0;
@@ -118,7 +123,13 @@ void GrInOrderDrawBuffer::drawRect(const GrRect& rect,
             combinedMatrix.preConcat(*matrix);
         }
 
-        SetRectVertices(rect, &combinedMatrix, srcRects, srcMatrices, layout, geo.vertices());
+        SetRectVertices(rect, &combinedMatrix, srcRects, srcMatrices, 
+                        this->getDrawState().getColor(), layout, geo.vertices());
+
+        // Now that the paint's color is stored in the vertices set it to
+        // white so that the following code can batch all the rects regardless
+        // of paint color
+        AutoColorRestore acr(this, SK_ColorWHITE);
 
         // we don't want to miss an opportunity to batch rects together
         // simply because the clip has changed if the clip doesn't affect

src/gpu/GrDrawTarget.cpp
@@ -1039,8 +1039,8 @@ void GrDrawTarget::drawRect(const GrRect& rect,
     }
 
     SetRectVertices(rect, matrix, srcRects,
-                    srcMatrices, layout, geo.vertices());
-
+                    srcMatrices, SK_ColorBLACK, layout, geo.vertices());
+    
     drawNonIndexed(kTriangleFan_GrPrimitiveType, 0, 4);
 }
 
@@ -1060,10 +1060,20 @@ GrVertexLayout GrDrawTarget::GetRectVertexLayout(const GrRect* srcRects[]) {
     return layout;
 }
 
+// This method fills int the four vertices for drawing 'rect'. 
+//      matrix - is applied to each vertex
+//      srcRects - provide the uvs for each vertex
+//      srcMatrices - are applied to the corresponding 'srcRect'
+//      color - vertex color (replicated in each vertex)
+//      layout - specifies which uvs and/or color are present
+//      vertices - storage for the resulting vertices
+// Note: the color parameter will only be used when kColor_VertexLayoutBit
+// is present in 'layout'
 void GrDrawTarget::SetRectVertices(const GrRect& rect,
                                    const GrMatrix* matrix,
                                    const GrRect* srcRects[],
                                    const GrMatrix* srcMatrices[],
+                                   GrColor color,
                                    GrVertexLayout layout,
                                    void* vertices) {
 #if GR_DEBUG
@@ -1077,9 +1087,9 @@ void GrDrawTarget::SetRectVertices(const GrRect& rect,
     }
 #endif
 
-    int stageOffsets[GrDrawState::kNumStages];
+    int stageOffsets[GrDrawState::kNumStages], colorOffset;
     int vsize = VertexSizeAndOffsetsByStage(layout, stageOffsets,
-                                            NULL, NULL, NULL);
+                                            &colorOffset, NULL, NULL);
 
     GrTCast<GrPoint*>(vertices)->setRectFan(rect.fLeft, rect.fTop,
                                             rect.fRight, rect.fBottom,
@@ -1098,6 +1108,16 @@ void GrDrawTarget::SetRectVertices(const GrRect& rect,
             if (NULL != srcMatrices && NULL != srcMatrices[i]) {
                 srcMatrices[i]->mapPointsWithStride(coords, vsize, 4);
             }
+        }
+    }
+
+    if (layout & kColor_VertexLayoutBit) {
+
+        GrColor* vertCol = GrTCast<GrColor*>(GrTCast<intptr_t>(vertices) + colorOffset);
+
+        for (int i = 0; i < 4; ++i) {
+            *vertCol = color;
+            vertCol = (GrColor*) ((intptr_t) vertCol + vsize);
         }
     }
 }

src/gpu/GrDrawTarget.h
@@ -627,6 +627,26 @@ public:
 
     ////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Constructor sets the color to be 'color' which is undone by the destructor.
+     */
+    class AutoColorRestore : public ::GrNoncopyable {
+    public:
+        AutoColorRestore(GrDrawTarget* target, GrColor color) {
+            fDrawTarget = target;
+            fOldColor = target->drawState()->getColor();
+            target->drawState()->setColor(color);
+        }
+        ~AutoColorRestore() {
+            fDrawTarget->drawState()->setColor(fOldColor);
+        }
+    private:
+        GrDrawTarget*   fDrawTarget;
+        GrColor         fOldColor;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+
     class AutoReleaseGeometry : ::GrNoncopyable {
     public:
         AutoReleaseGeometry(GrDrawTarget*  target,
@@ -1023,6 +1043,7 @@ protected:
                                 const GrMatrix* matrix,
                                 const GrRect* srcRects[],
                                 const GrMatrix* srcMatrices[],
+                                GrColor color,
                                 GrVertexLayout layout,
                                 void* vertices);
 

commit 464aa3fbabc64b903baa1a59f482332d47796451

Fixed xoom bug introduced in r5830
https://codereview.appspot.com/6604053/



git-svn-id: https://skia.googlecode.com/svn/trunk@5862 2bbb7eff-a529-9590-31e7-b0007b416f81

src/gpu/GrInOrderDrawBuffer.cpp
@@ -92,9 +92,34 @@ void GrInOrderDrawBuffer::drawRect(const GrRect& rect,
         bool appendToPreviousDraw = false;
         GrVertexLayout layout = GetRectVertexLayout(srcRects);
 
-        // When we batch rects we store the color at each vertex in order
-        // to allow batching when only the draw color is changing (the usual case)
-        layout |= kColor_VertexLayoutBit;
+        // Batching across colors means we move the draw color into the
+        // rect's vertex colors to allow greater batching (a lot of rects
+        // in a row differing only in color is a common occurence in tables).
+        bool batchAcrossColors = true;
+        if (!this->getCaps().dualSourceBlendingSupport()) {
+            for (int s = 0; s < GrDrawState::kNumStages; ++s) {
+                if (this->getDrawState().isStageEnabled(s)) {
+                    // We disable batching across colors when there is a texture 
+                    // present because (by pushing the the color to the vertices)
+                    // Ganesh loses track of the rect's opacity. This, in turn, can
+                    // cause some of the blending optimizations to be disabled. This
+                    // becomes a huge problem on some of the smaller devices where
+                    // shader derivatives and dual source blending aren't supported.
+                    // In those cases paths are often drawn to a texture and then
+                    // drawn as a texture (using this method). Because dual source
+                    // blending is disabled (and the blend optimizations are short
+                    // circuited) some of the more esoteric blend modes can no longer
+                    // be supported.
+                    // TODO: add tracking of batchAcrossColors's opacity
+                    batchAcrossColors = false;
+                    break;
+                }
+            }
+        }
+
+        if (batchAcrossColors) {
+            layout |= kColor_VertexLayoutBit;
+        }
 
         AutoReleaseGeometry geo(this, layout, 4, 0);
         if (!geo.succeeded()) {
@@ -129,7 +154,9 @@ void GrInOrderDrawBuffer::drawRect(const GrRect& rect,
         // Now that the paint's color is stored in the vertices set it to
         // white so that the following code can batch all the rects regardless
         // of paint color
-        GrDrawState::AutoColorRestore acr(this->drawState(), SK_ColorWHITE);
+        GrDrawState::AutoColorRestore acr(this->drawState(), 
+                                          batchAcrossColors ? SK_ColorWHITE
+                                                            : this->getDrawState().getColor());
 
         // we don't want to miss an opportunity to batch rects together
         // simply because the clip has changed if the clip doesn't affect